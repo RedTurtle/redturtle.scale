48cbdf2f16981cd13f6e9302fa73687cc6339868
Mauro Amico <mauro.amico@gmail.com>
2025-04-28 10:58:08 +0200

    init

---
+++ src/redturtle/scale/scale.py
@@ -0,0 +1,74 @@
+from plone.scale.scale import scalePILImage
+import logging
+import io
+from plone.scale import scale
+
+logger = logging.getLogger(__name__)
+
+def scaleSingleFrame(
+    image,
+    width,
+    height,
+    mode,
+    format_,
+    quality,
+    direction,
+):
+    image = scalePILImage(image, width, height, mode, direction=direction)
+    # convert to simpler mode if possible
+    colors = image.getcolors(maxcolors=256)
+    if image.mode not in ("P", "L", "LA") and colors:
+        if format_ == "JPEG":
+            # check if it's all grey
+            if all(rgb[0] == rgb[1] == rgb[2] for c, rgb in colors):
+                image = image.convert("L")
+        elif format_ in ("PNG", "GIF"):
+            image = image.convert("P")
+    if image.mode == "RGBA" and format_ == "JPEG":
+        extrema = dict(zip(image.getbands(), image.getextrema()))
+        if extrema.get("A") == (255, 255):
+            # no alpha used, just change the mode, which causes the alpha band
+            # to be dropped on save
+            image = image.convert("RGB")
+        else:
+            # switch to PNG, which supports alpha
+            format_ = "PNG"
+    # XXX: force to WEBP format
+    if format_ in ["JPEG", "PNG"]:
+        format_ = "WEBP"
+    # --- DEBUG
+    sizes = {}
+    for t in set([format_, "WEBP", "JPEG", "PNG"]):
+        try:
+            out = io.BytesIO()
+            image.save(
+                out, 
+                format=t,
+                quality=88,
+                optimize=True,
+                progressive=True,
+            )
+            sizes[t] = out.tell()
+        except OSError:
+            sizes[t] = "---"
+    print(sizes)
+    # --- DEBUG
+    return image, format_
+
+def apply_patches():
+    logger.info("monkeypatch plone.scale.scale.scaleSingleFrame")
+    scale.scaleSingleFrame = scaleSingleFrame
+
+
+# DEBUG
+# from plone.namedfile import file
+# from time import time
+# def wrap(fun):
+#     def inner(*args, **kwargs):
+#         t = time()
+#         ret = fun(*args, **kwargs)
+#         logger.info("[%sms] %s %s => %s", int(((time() - t) * 1000)), args, kwargs, ret)
+#         return ret
+#     return inner
+# file.getImageInfo = wrap(file.getImageInfo)
+
---
+++ src/redturtle/scale/storage.py
@@ -0,0 +1,28 @@
+from plone.scale.storage import AnnotationStorage as Base
+import logging
+logger = logging.getLogger(__name__)
+
+
+class AnnotationStorage(Base):
+    def hash_key(self, **parameters):
+        key = super().hash_key(**parameters)
+        fieldname = parameters.get("fieldname")
+        if fieldname:
+            field = getattr(self.context, 'image', None)
+            if getattr(field, "contentType", None) in ("image/jpeg", "image/png"):
+                key = f"{key}-webp"
+        logger.debug("hash_key %s %s -> %s", self, parameters, key)
+        return key
+
+    def hash(self, **parameters):
+        key = super().hash(**parameters)
+        fieldname = parameters.get("fieldname")
+        if fieldname:
+            field = getattr(self.context, 'image', None)
+            if getattr(field, "contentType", None) in ("image/jpeg", "image/png"):
+                key = key + (("_format", "webp"), )
+        logger.debug("key %s %s -> %s", self, parameters, key)
+        return key
+
+    def unhash(self, hash_key):
+        return dict(item for item in hash_key if not item[0].startswith("_"))
