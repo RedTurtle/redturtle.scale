68e4f5dd8d77f7067fb969d373fb122377dcc5ce
Mauro Amico <mauro.amico@gmail.com>
2025-05-22 09:02:59 +0200

    ci

--- src/redturtle/scale/setuphandlers.py
+++ src/redturtle/scale/setuphandlers.py
@@ -8,7 +8,6 @@ from zope.interface import implementer
 
 @implementer(INonInstallable)
 class HiddenProfiles(object):
-
     def getNonInstallableProfiles(self):
         """Hide uninstall profile from site-creation and quickinstaller."""
         return [
